50a3d7b65f3136f791728b141b0a95bb025f2e31
sic1203 <42786352+sic1203@users.noreply.github.com>
2018-09-07 15:59:26 -0400

    SiChen_sic2_hw1

---
+++ SiChen1.R
@@ -0,0 +1,75 @@
+#Si Chen
+#Assignment 1
+
+library(shiny)
+library(reshape2)
+library(dplyr)
+library(plotly)
+library(shinythemes)
+require(RColorBrewer)
+
+# datasets: ToothGrowth
+#A data frame with 60 observations on 3 variables.
+#description:The response is the length of odontoblasts in 60 guinea pigs. 
+# Each animal received one of three dose levels of vitamin C by one of two delivery methods, orange juice or ascorbic acid 
+# len	numeric	Tooth length
+# supp	factor	Supplement type (VC or OJ).
+# dose	numeric	Dose in milligrams/day
+
+tg <- ToothGrowth
+melttg <- melt(tg, id = "supp")
+melttg$supp <- as.factor(melttg$supp)
+pdf(NULL)
+
+#input1
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  navbarPage("The Effect of Vitamin C on Tooth Growth", 
+             theme = shinytheme("united"),
+             tabPanel("Plot",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("char_select",
+                                      "Supplement type:",
+                                      choices = levels(melttg$supp),
+                                      multiple = TRUE,
+                                      selectize = TRUE,
+                                      selected = c("VC", "OJ")),
+                          #input 2
+                          radioButtons("supp", "Please select the supplement type:", choices = c("OJ","VC"))
+                        ),
+                        # Output plot
+                        mainPanel(
+                          plotlyOutput("plot")
+                        )
+                      )
+             ),
+             # Data Table
+             tabPanel("Table",
+                      fluidPage(DT::dataTableOutput("table"))
+             )
+  )
+)
+
+# Define server logic
+server <- function(input, output) {
+  output$plot <- renderPlotly({
+    dat <- subset(melttg, supp %in% input$char_select)
+    ggplot(data = dat, aes(x = supp, y = as.numeric(value), fill = supp)) + geom_point(stat = "identity")+
+      ggtitle("Value distribution of different supplement type") +xlab("Supplement Type") + ylab("Value")+ 
+      scale_fill_brewer(palette="Set2")
+                                                       
+  })
+  output$table <- DT::renderDataTable({
+    subset(tg, supp %in% input$char_select, select = c(supp, len,dose))
+  })
+}
+
+
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
+
